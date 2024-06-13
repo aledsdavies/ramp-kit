a0631b7999597c54cade24477926d8eaec6874b6
Aled Davies <aledsdavies@gmail.com>
2024-06-13 16:38:09 +0100

    Initial setup for Templ with Go
    - Implemented basic template rendering with Go.
    - Set up an initial HTML layout to support meta property injection.
    - Enabled dynamic setting of title, description, and metadata.

--- internal/routing/router.go
+++ internal/routing/router.go
@@ -3,8 +3,12 @@ package routing
 import (
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+
+	"github.com/asdavies/auth/views"
 )
 
 func NewRouter() *chi.Mux {
@@ -14,7 +18,11 @@ func NewRouter() *chi.Mux {
     r.Use(middleware.Recoverer)
 
     r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello, World!!"))
+        http.Redirect(w, r,  "/login", http.StatusSeeOther)
+    })
+
+    r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+        templ.Handler(views.Index("Aled")).ServeHTTP(w,r)
     })
 
     return r
